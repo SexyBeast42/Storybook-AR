678046414a0b76b23304f095b41aa2d26b474abc
SexyBeast42 <davakoeswoyo11@gmail.com>
2023-09-29 05:23:25 +0200

    PhotoLogicManager.cs finished.
    Player can now take a photo, then edit the photo to be used for AR minigames.
    
    Still needs to be connected to actual models to be displayed on the phone, and for further iterations on the AR mechanics to see if it will recognize the photo.
    
    Notion: This update is for 1st prototype> Game Mechanics> AR Recognition Mechanics> Scannable Drawings.

---
+++ Assets/ImageMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class ImageMovement : MonoBehaviour
+{
+    public LayerMask excludedLayer;
+    
+    void Update()
+    {
+        for (var i = 0; i < Input.touchCount; i++)
+        {
+            if (!IsPointerOverUIObject() && Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+                // assign new position to where finger was pressed
+                transform.position = new Vector3 (Input.GetTouch(i).position.x, Input.GetTouch(i).position.y, transform.position.z);
+            }
+        }
+    }
+    
+    private bool IsPointerOverUIObject() {
+        // Referencing this code for GraphicRaycaster https://gist.github.com/stramit/ead7ca1f432f3c0f181f
+        // the ray cast appears to require only eventData.position.
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+ 
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        
+        results.RemoveAll(result => (1 << result.gameObject.layer & excludedLayer) != 0); // Exclude ignoreraycast
+        
+        return results.Count > 0;
+    }
+}
--- Assets/Scripts/Image Capture Scripts/PhotoLogicManager.cs
+++ Assets/Scripts/Image Capture Scripts/PhotoLogicManager.cs
@@ -1,7 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 /// <summary>
 /// Allow the player to take a photo and access their photo library. Editing the photo, then saving the photo to be used
@@ -11,90 +16,129 @@ using UnityEngine;
 [Serializable]
 public class ARPhotoID
 {
-    public string arID;
+    public string name;
     public Texture2D photoID;
+    public Sprite editedPhotoID;
     public GameObject modelID;
-    public bool isEdited;
 }
 
 public class CurrentEdit
 {
-    public Texture2D currentPhoto;
+    public int number, rotation;
 }
 
 public class PhotoLogicManager : MonoBehaviour
 {
     public List<ARPhotoID> arPhotoList = new List<ARPhotoID>();
-    public List<GameObject> prefabList = new List<GameObject>();
-    public CurrentEdit CurrentEdit;
-    
-    private bool isTakingPhoto;
-    private bool isGettingPhotos;
+    public CurrentEdit CurrentEdit = new CurrentEdit();
 
+    private bool isTakingPhoto;
+
+    public GameObject takeUI, chooseUI, editUI, saveUI;
+
+    [SerializeField] private TMP_Text chooseText;
+    
+    public Image imageDisplayed, boxOutline;
+    private RectTransform imageDisplayedRect;
+    public Slider sizeSlider;
+
+    private enum State
+    {
+        Snapping,
+        Choose,
+        Editing,
+        Finished
+    };
+
+    private State _state;
+    
     void Awake()
     {
         isTakingPhoto = false;
-        isGettingPhotos = false;
-    }
 
+        ToggleTakeUI(false);
+        ToggleChooseUI(false);
+        ToggleEditUI(false);
+        
+        imageDisplayedRect = imageDisplayed.GetComponent<RectTransform>();
+
+        _state = State.Snapping;
+    }
+    
     void Update()
     {
-        if (isTakingPhoto) return;
-    }
-
-    // Call this function before calling any other function first
-    public async void RequestPermissionAsynchronously( NativeGallery.PermissionType permissionType, NativeGallery.MediaType mediaTypes )
-    {
-        NativeGallery.Permission permission = await NativeGallery.RequestPermissionAsync( permissionType, mediaTypes );
-        Debug.Log( "Permission result: " + permission );
-    }
-    
-    public void GetPhotosFromLibrary()
-    {
-        if (!NativeGallery.CanSelectMultipleFilesFromGallery()) return; // Check if device is able to select files
-        
-        int maxSize = 512;
-        
-        NativeGallery.Permission permission = NativeGallery.GetImagesFromGallery( ( paths ) =>
+        switch (_state)
         {
-            if (paths == null) return;
-
-            if (paths.Length != arPhotoList.Count) return; // flag amount
-
-            for (int i = 0; i < paths.Length; i++)
-            {
-                // Create Texture from selected image
-                Texture2D texture = NativeGallery.LoadImageAtPath(paths[i], maxSize);
+            case State.Snapping:
+                if (!isTakingPhoto)
+                {
+                    ToggleTakeUI(true);
+                }
+                
+                if (CurrentEdit.number == arPhotoList.Count())
+                {
+                    ToggleTakeUI(false);
+                    
+                    CurrentEdit.number = 0;
+                    
+                    _state = State.Choose;
+                }
+                
+                break;
             
-                if( texture == null ) return;
+            case State.Choose:
+                ToggleChooseUI(true);
+                
+                if (CurrentEdit.number == arPhotoList.Count())
+                {
+                    ToggleChooseUI(false);
+                    
+                    CurrentEdit.number = 0;
+                    
+                    _state = State.Editing;
+                }
+                
+                chooseText.text = "Choose the drawing for the " + arPhotoList[CurrentEdit.number].name;
+                
+                break;
             
-                CurrentEdit.currentPhoto = texture;
-            }
-        } );
-    }
-
-    IEnumerator GetPhotosFromLibraryCoroutine(int seconds)
-    {
-        isGettingPhotos = true;
-        
-        yield return new WaitForSeconds(seconds);
-        
-        GetPhotosFromLibrary();
-
-        isGettingPhotos = false;
+            case State.Editing:
+                if (!isTakingPhoto)
+                {
+                    ToggleEditUI(true);
+                }
+                
+                ChangePhotoSize(sizeSlider.value);
+                
+                DisplayPhoto();
+                
+                if (CurrentEdit.number == arPhotoList.Count())
+                {
+                    ToggleEditUI(false);
+                    
+                    _state = State.Finished;
+                }
+                
+                break;
+            
+            case State.Finished:
+                
+                break;
+        }
     }
     
-    public void TakePhoto(string photoName)
+    public void TakePhoto()
     {
         if (isTakingPhoto) return;
+        
+        isTakingPhoto = true;
+        ToggleTakeUI(false);
 
-        StartCoroutine(TakePhotoCoroutine(photoName));
+        StartCoroutine(TakePhotoCoroutine());
     }
 
-    IEnumerator TakePhotoCoroutine(string photoName)
+    IEnumerator TakePhotoCoroutine()
     {
-        isTakingPhoto = true;
-
         yield return new WaitForEndOfFrame();
         
         // Takes a photo
@@ -103,10 +147,128 @@ public class PhotoLogicManager : MonoBehaviour
         photo.Apply();
         
         // Save photo to Gallery/Photos
-        NativeGallery.Permission permission = NativeGallery.SaveImageToGallery(photo, "ARStorybook", photoName + ".png", ( success, path ) => Debug.Log( "Media save result: " + success + " " + path ));
+        NativeGallery.Permission permission = NativeGallery.SaveImageToGallery(photo, 
+            "ARStorybook", 
+            arPhotoList[CurrentEdit.number].name + ".png", 
+            ( success, path ) => Debug.Log( "Media save result: " + success + " " + path ));
         
         Destroy(photo); // Avoid memory leaks
 
+        CurrentEdit.number++;
+        
+        ToggleTakeUI(true);
         isTakingPhoto = false;
     }
+        
+    public void GetPhotoFromLibrary()
+    {
+        int maxSize = 512;
+        
+        NativeGallery.Permission permission = NativeGallery.GetImageFromGallery( ( path ) =>
+        {
+            if (path == null) return;
+            
+            // Create Texture from selected image
+            Texture2D texture = NativeGallery.LoadImageAtPath(path, maxSize);
+            
+            if (texture == null) return;
+            
+            arPhotoList[CurrentEdit.number].photoID = texture;
+
+            CurrentEdit.number++;
+        } );
+    }
+
+    public void DisplayPhoto()
+    {
+        if (CurrentEdit.number == arPhotoList.Count) return;
+        
+        Texture2D currentTexture = arPhotoList[CurrentEdit.number].photoID;
+        
+        Sprite currentSprite = Sprite.Create(currentTexture,
+            new Rect(0, 0, currentTexture.width, currentTexture.height), Vector2.one * 0.5f); // Convert to sprite
+        
+        imageDisplayedRect.sizeDelta = new Vector2(currentTexture.width, currentTexture.height);
+        
+        imageDisplayed.sprite = currentSprite;
+    }
+
+    public void ChangePhotoSize(float newSize)
+    {
+        imageDisplayed.transform.localScale = new Vector3(newSize, newSize, newSize);
+    }
+
+    public void ChangePhotoRotation()
+    {
+        int rotationIncrement = 90;
+        CurrentEdit.rotation += rotationIncrement;
+        
+        imageDisplayed.transform.rotation = Quaternion.Euler(0f, 0f, CurrentEdit.rotation);
+    }
+
+    public void SavePhoto()
+    {
+        if (isTakingPhoto || CurrentEdit.number == arPhotoList.Count) return;
+
+        isTakingPhoto = true;
+        ToggleSaveUI(false);
+
+        StartCoroutine(SavePhotoCoroutine());
+    }
+    
+    IEnumerator SavePhotoCoroutine()
+    {
+        yield return new WaitForEndOfFrame();
+
+        int width = (int)boxOutline.sprite.rect.width;
+        int height = (int)boxOutline.sprite.rect.height;
+        
+        // Takes a photo
+        Texture2D photo = new Texture2D(width, height, TextureFormat.RGB24, false);
+
+        Rect rect = new Rect(boxOutline.rectTransform.transform.position.x - width/2, boxOutline.rectTransform.transform.position.y - height/2, width, height);
+        
+        photo.ReadPixels(rect, 0, 0);
+        photo.Apply();
+        
+        // Save photo to Gallery/Photos
+        NativeGallery.Permission permission = NativeGallery.SaveImageToGallery(photo, 
+            "ARStorybook", 
+            arPhotoList[CurrentEdit.number].name + "edited.png", 
+            ( success, path ) => Debug.Log( "Media save result: " + success + " " + path ));
+        
+        Sprite currentSprite = Sprite.Create(photo,
+            new Rect(0, 0, photo.width, photo.height), Vector2.one * 0.5f); // Convert to sprite
+
+        arPhotoList[CurrentEdit.number].editedPhotoID = currentSprite;
+        
+        Destroy(photo); // Avoid memory leaks
+        Destroy(currentSprite);
+
+        CurrentEdit.number++;
+        
+        ToggleSaveUI(true);
+        isTakingPhoto = false;
+    }
+    
+    private void ToggleTakeUI(bool uiState)
+    {
+        takeUI.SetActive(uiState);
+    }
+
+    private void ToggleChooseUI(bool uiState)
+    {
+        chooseUI.SetActive(uiState);
+    }
+    
+    private void ToggleEditUI(bool uiState)
+    {
+        editUI.SetActive(uiState);
+    }
+
+    private void ToggleSaveUI(bool uiState)
+    {
+        saveUI.SetActive(uiState);
+    }
+    
 }
